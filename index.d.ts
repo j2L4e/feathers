0d04913040a706bffd6a84d5e8814c8be1de94a7
Jan Lohage <git@j2L4e.de>
2017-07-20 16:18:43 +0200

    improve typings

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import * as expressCore from "express-serve-static-core";
+import * as expressCore from 'express-serve-static-core';
 import * as events from 'events';
 
 declare function feathers<ServiceList>(): feathers.Application<ServiceList>;
@@ -21,53 +21,77 @@ declare namespace feathers {
     data: T[]
   }
 
-  interface Service<T> extends events.EventEmitter {
-
+  interface GetService<T> {
     /**
      * Retrieves a list of all resources from the service.
      * Provider parameters will be passed as params.query
      */
-    find?(params?: Params, callback?: any): Promise<T[] | Pagination<T>>;
+    find(params?: Params, callback?: any): Promise<T[] | Pagination<T>>;
+  }
 
+  interface FindService<T> {
     /**
      * Retrieves a single resource with the given id from the service.
      */
-    get?(id: number | string, params?: Params, callback?: any): Promise<T>;
+    get(id: number | string, params?: Params, callback?: any): Promise<T>;
+  }
 
+  interface CreateService<T> {
     /**
      * Creates a new resource with data.
      */
-    create?(data: T | T[], params?: Params, callback?: any): Promise<T | T[]>;
+    create(data: T[], params?: Params, callback?: any): Promise<T[]>;
+    create(data: T, params?: Params, callback?: any): Promise<T>;
+  }
 
+  interface UpdateService<T> {
     /**
      * Replaces the resource identified by id with data.
      * Update multiples resources with id equal `null`
      */
-    update?(id: NullableId, data: T, params?: Params, callback?: any): Promise<T>;
+    update(id: NullableId, data: T, params?: Params, callback?: any): Promise<T>;
+  }
 
+  interface PatchService<T> {
     /**
      * Merges the existing data of the resource identified by id with the new data.
      * Implement patch additionally to update if you want to separate between partial and full updates and support the PATCH HTTP method.
      * Patch multiples resources with id equal `null`
      */
-    patch?(id: NullableId, data: any, params?: Params, callback?: any): Promise<T>;
+    patch(id: NullableId, data: any, params?: Params, callback?: any): Promise<T>;
+  }
 
+  interface RemoveService<T> {
     /**
      * Removes the resource with id.
      * Delete multiple resources with id equal `null`
      */
-    remove?(id: NullableId, params?: Params, callback?: any): Promise<T>;
-
-    /**
-     * Initialize your service with any special configuration or if connecting services that are very tightly coupled
-     */
-    setup?(app?: Application<any>, path?: string): void;
-
-    before(any?: any): this;
-    after(any?: any): this;
-    filter(any?: any): this;
-    
+    remove(id: NullableId, params?: Params, callback?: any): Promise<T>;
   }
+
+  interface OptionalServiceMethods <T> {
+    find?(params?: Params, callback?: any): Promise<T[] | Pagination<T>>;
+    get?(id: number | string, params?: Params, callback?: any): Promise<T>;
+    create?(data: T[], params?: Params, callback?: any): Promise<T[]>;
+    create?(data: T, params?: Params, callback?: any): Promise<T>;
+    update?(id: NullableId, data: T, params?: Params, callback?: any): Promise<T>;
+    patch?(id: NullableId, data: any, params?: Params, callback?: any): Promise<T>;
+    remove?(id: NullableId, params?: Params, callback?: any): Promise<T>;
+  }
+
+  interface ServiceCore<T> extends OptionalServiceMethods<T> {
+    setup?(app?: Application<any>, path?: string): void;
+  }
+
+  interface ServiceAddons extends events.EventEmitter {
+    filter(any?: any): this;
+  }
+
+  interface PartialServiceCore<T> extends OptionalServiceMethods<T>{}
+  interface FullServiceCore<T> extends GetService<T>, FindService<T>, CreateService<T>, UpdateService<T>, PatchService<T>, RemoveService<T> {}
+
+  type FullOrPartialServiceCore<T> = FullServiceCore<T> | PartialServiceCore<T>;
+  type Service<T> = FullOrPartialServiceCore<T> & ServiceAddons;
 
   interface FeathersUseHandler<T> extends expressCore.IRouterHandler<T>, express.IRouterMatcher<T> {
     (location: string, service: Service<any>): T
@@ -77,12 +101,12 @@ declare namespace feathers {
     /**
      * It either returns the Feathers wrapped service object for the given path
      */
-    service<T>(fn: (services: ServiceList) => Service<T>): Service<T>;
     service<T>(location: string): Service<T>;
 
     /**
      * Registers a new service for that path and returns the wrapped service object
      */
+    service<T>(fn: (services: ServiceList) => T): T & ServiceAddons;
     service<T>(location: string, service: Service<T>, options?: any): Service<T>;
 
     /**
@@ -94,6 +118,11 @@ declare namespace feathers {
      * Register a service object
      */
     use: FeathersUseHandler<this>;
+
+    /**
+     * Runs a callback function with the application as the context (this). It can be used to initialize plugins or services.
+     */
+    configure(callback: any): this;
   }
 }
 
